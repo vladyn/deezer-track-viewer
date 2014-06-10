7b2684300dcb24721f8a7c0bbb9ab0c528ace8c5
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2014-06-10 15:13:31 +0300

    feature ends

--- app/js/app.js
+++ app/js/app.js
@@ -9,12 +9,11 @@ angular.module('myApp', [
     'myApp.directives',
     'myApp.controllers'
 ]).config(['$routeProvider', '$httpProvider', '$locationProvider', function($routeProvider, $httpProvider, $locationProvider) {
-    $routeProvider.when('/app/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/app/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/app/deezer', {templateUrl: 'partials/deezer.html', controller: 'WidgetController'});
-    $routeProvider.when('/app/', {templateUrl: 'partials/deezer.html', controller: 'WidgetController'});
-    $routeProvider.otherwise({redirectTo: '/app/'});
+    $routeProvider.when('app/deezer/', {templateUrl: 'partials/deezer.html', controller: 'WidgetController'});
+    $routeProvider.when('app/', {templateUrl: 'partials/deezer.html', controller: 'WidgetController'});
+    $routeProvider.when('app/hello-world', {templateUrl: 'index.html', controller: 'HelloCtrl'});
+    $routeProvider.otherwise({redirectTo: 'app/'});
 	$httpProvider.defaults.useXDomain = true;
 	delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    $locationProvider.html5Mode(true).hashPrefix('!');
+    $locationProvider.html5Mode(true);
 }]);
