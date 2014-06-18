6e73c9d56301f3187c99796df79cf8f3c121eab2
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2014-06-18 17:35:08 +0300

    added bootstrap

--- app/js/services.js
+++ app/js/services.js
@@ -1,33 +1,24 @@
 'use strict';
 
 /* Services */
-
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
+'use strict';
 angular.module('myApp.services', [])
 .value('version', '0.1')
-// .provider('deezerConnect', [function () {
+.provider('deezerConnect', [function (token, cookie, url, format) {
 
-// 	var defaultToken  = 'frcE5cMznk538ee2ce928dcJEqOByIC538ee2ce92919RSsOif',
-// 	cName  = 'X-XSRF-TOKEN',
-// 	url    = 'http://api.deezer.com/user/17673156/personal_songs',
-// 	config = {
-// 		params: {
-// 		  access_token: token,
-// 		  callback: 'JSON_CALLBACK', 
-// 		  output: 'jsonp'
-// 		}
-// 	};
+	var defaultToken  = 'frcE5cMznk538ee2ce928dcJEqOByIC538ee2ce92919RSsOif';
+	var cName  = 'X-XSRF-TOKEN';
+	var url    = 'http://api.deezer.com/user/17673156/personal_songs';
+	var config = {
+        access_token: defaultToken,
+        callback: 'JSON_CALLBACK', 
+        output: 'jsonp'
+	};
 
-// 	this.token = defaultToken;
-	
+	this.token = defaultToken;
 
-
-// 	this.$get = [function() {
-// 		return {
-// 		};
-// 	}];
-// }])
-
-
+	this.$get = [function() {
+		return {
+		};
+	}];
+}]);
