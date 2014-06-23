b57a7f33a635223ece1e864ed5c6cd018d7679f5
Vladimir Varbanov <vladimir.varbanov@paddypower.com>
2014-06-23 17:08:08 +0300

    filter results added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,7 +17,7 @@ angular.module('myApp.controllers', [])
   .controller('WidgetController', ['$http', '$log', '$scope', 'deezerConnect', function($http, $log, $scope, deezerConnect){
     // deezerConnect
   var token  = 'frcE5cMznk538ee2ce928dcJEqOByIC538ee2ce92919RSsOif',
-      url    = 'http://api.deezer.com/user/17673156/personal_songs',
+      url    = 'http://api.deezer.com/user/17673156/tracks',
       config = {
           params: {
               access_token: token,
@@ -26,7 +26,7 @@ angular.module('myApp.controllers', [])
           }
       };
 
-  	$http.jsonp(url, config)
+  	deezerConnect.get(url, config)
       .success(function(data) {
         $scope.songs       = data.data;
         $scope.total       = data.total;
@@ -36,8 +36,7 @@ angular.module('myApp.controllers', [])
         $log.info("Something went wrong: " + data + status);
       });
 
-      $scope.alertMe = function(msg) {
-        window.open("personal_songs.json");
+      $scope.switchItems = function(item) {
       };
   }]);
 
--- app/js/services.js
+++ app/js/services.js
@@ -3,20 +3,13 @@
 /* Services */
 'use strict';
 angular.module('myApp.services', [])
-.value('version', '0.1')
-.factory('deezerConnect', [function () {
-	var defaultToken  = 'frcE5cMznk538ee2ce928dcJEqOByIC538ee2ce92919RSsOif';
-	var url    = 'http://api.deezer.com/user/17673156/personal_songs';
-	var config = {
-        access_token: defaultToken,
-        callback: 'JSON_CALLBACK', 
-        output: 'jsonp'
-	};
-
+.factory('deezerConnect', ['$http', function ($http) {
 	return {
-        get: function(token, url, format) {
-            this.token = token || defaultToken;
+        get: function (url, config) {
+            return $http.jsonp(url, config);
         },
-        post: function() {}
+        delete: function(item) {
+            return $http.post(url, item, config);
+        }
     };
 }]);
